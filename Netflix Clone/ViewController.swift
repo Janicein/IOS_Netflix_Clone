e55bb79170ce08e658a7b1b03f26cec20c37d724
Jiaming Zhou <zhou.jiam@northeastern.edu>
2023-07-04 15:48:21 +0800

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by 周嘉茗 on 2023/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
