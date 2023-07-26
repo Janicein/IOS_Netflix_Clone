7b51d69aaebc3b4a98fb09d10bee90a969fe69c7
Jiaming Zhou <zhou.jiam@northeastern.edu>
2023-07-26 16:36:04 +0800

    Add searchBar in search page and add SearchResultViewController

--- Netflix Clone/Controller/Core/SearchViewController.swift
+++ Netflix Clone/Controller/Core/SearchViewController.swift
@@ -16,6 +16,14 @@ class SearchViewController: UIViewController {
         table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
         return table
     }()
+    
+    
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultsViewController())
+        controller.searchBar.placeholder = "Search for a Movie or a Tv show"
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +37,8 @@ class SearchViewController: UIViewController {
         view.addSubview(discoverTable)
         discoverTable.delegate = self
         discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
         
         fetchDiscoverMovies()
     }
---
+++ Netflix Clone/Controller/General/SearchResultsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by 周嘉茗 on 2023/7/26.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGreen
+    }
+    
+
+
+
+}
