9e5c2f32465db2213b0352b05fccc2fddd54a93c
Jiaming Zhou <zhou.jiam@northeastern.edu>
2023-08-10 16:52:44 +0800

    Create DataModel and DataPersistenceManager

---
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -0,0 +1,20 @@
+//
+//  DataPersistenceManager.swift
+//  Netflix Clone
+//
+//  Created by 周嘉茗 on 2023/8/10.
+//
+
+import Foundation
+
+
+class DataPersistenceManager {
+    
+    
+    static let shared = DataPersistenceManager()
+    
+    func downloadTitleWith(model: Title, completion: @escaping (Result<Void, Error>) -> Void){
+        
+        
+    }
+}
--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -52,6 +52,12 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath){
+
+        print("Dowdloading \(titles[indexPath.row].original_title)")
+        
+    }
 
 }
 
@@ -106,9 +112,9 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate,UICollectionView
             
             let config = UIContextMenuConfiguration(
                 identifier: nil,
-                previewProvider: nil){ _ in
+                previewProvider: nil){[weak self] _ in
                     let downloadAction = UIAction(title: "Download", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off){ _ in
-                        print("Download tapped")
+                        self?.downloadTitleAt(indexPath: indexPath)
                     }
                     return UIMenu(title: "", image: nil, identifier:  nil, options: .displayInline, children: [downloadAction])
                 }
